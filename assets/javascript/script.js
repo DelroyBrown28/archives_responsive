9c22f540fc71953c7b86887c2e327efc86f2b922
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-07-02 16:16:09 +0100

    desktop size for main page complete

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -13,4 +13,37 @@ $(document).ready(function () {
             type: 'progressbar',
         },
     })
+
+
+    // Local time display
+    function addZero(i) {
+        if (i < 10) {
+            i = "0" + i;
+        }
+        return i;
+    }
+
+    function timeUpdate() {
+        var grabDate = new Date();
+        var theTime = document.getElementById("the_time");
+        var hours = addZero(grabDate.getHours());
+        var minutes = addZero(grabDate.getMinutes());
+        var seconds = addZero(grabDate.getSeconds());
+        theTime.innerHTML = hours + ":" + minutes + ":" + seconds;
+    }
+    setInterval(timeUpdate, 1000);
+
+    // Takes you to The Crisis
+    $('.crisis_tab').click(function () {
+        $('.tab_fade').animate({
+            opacity: '0'
+        }, 200, function () {
+            $('.top_contents_index').delay(500).animate({
+                height: '0'
+            }, 200, function () {
+                location.href = 'the_crisis.html';
+            })
+        })
+    });
+
 })
