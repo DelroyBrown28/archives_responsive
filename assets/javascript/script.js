7204e20f55046328bb39ca5ec91780e82cddeb05
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-07-02 14:13:51 +0100

    main page complete for mobile device

---
+++ assets/javascript/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    // swiper js for tabs
+
+    var mySwiper = new Swiper('.swiper-container', {
+
+        direction: 'vertical',
+        loop: false,
+        slidesPerView: 'auto',
+        centeredSlides: true,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'progressbar',
+        },
+    })
+})
